347fc65c0476f53cab42893c9d1b078660b25901
pragyaChauhan240119 <127924894+pragyaChauhan240119@users.noreply.github.com>
2026-08-18 20:57:12 +0530

    Updated SkillsController to display skills from the database instead of hardcoded data.

--- PragyaPortfolio.Web/Controllers/SkillsController.cs
+++ PragyaPortfolio.Web/Controllers/SkillsController.cs
@@ -1,32 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using PragyaPortfolio.Web.Data;
 
 namespace PragyaPortfolio.Web.Controllers
 {
     public class SkillsController : Controller
     {
+        private readonly PortfolioDbContext _context;
+
+        public SkillsController(PortfolioDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            var skills  = new List<Models.Skill>
-            {
-                new Models.Skill
-                {Name = "Git",
-                Category = "DevOps",
-                Level = "Beginner"},
-
-                new Models.Skill{ Name = "GitHub",
-                Category = "DevOps",
-                Level = "Beginner"},
-
-                new Models.Skill{ Name = "ASP.NET Core",
-                Category = "Backend",
-                Level = "Learning"},
-
-                new Models.Skill{  Name = "C#",
-                Category = "Backend",
-                Level = "Learning"}
-
-
-            };
+            var skills = _context.Skills.ToList();
             return View(skills);
         }
     }
