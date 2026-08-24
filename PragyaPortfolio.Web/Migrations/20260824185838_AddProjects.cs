9869d1504f7d65f32361ee1f65a786281abe5686
pragyaChauhan240119 <127924894+pragyaChauhan240119@users.noreply.github.com>
2026-08-25 00:34:06 +0530

    Create Project Database Model

---
+++ PragyaPortfolio.Web/Migrations/20260824185838_AddProjects.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PragyaPortfolio.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Technologies = table.Column<string>(type: "TEXT", nullable: false),
+                    GithubLink = table.Column<string>(type: "TEXT", nullable: false),
+                    LiveDemoLink = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
